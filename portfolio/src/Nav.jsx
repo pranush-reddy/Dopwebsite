3d4fd474d8b6279d8f2e070d0d4f98895d52926d
pranush-reddy <pranush.reddy0102@gmail.com>
2025-10-29 00:04:47 +0530

    hero page , contact , skill section done

--- portfolio/src/Nav.jsx
+++ portfolio/src/Nav.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import sign from "./assets/sign.png"; 
 import './Nav.css';
 
 function Nav() {
@@ -13,29 +13,29 @@ function Nav() {
     <>
       <nav className="navbar">
         <div className="logo">
-          <h2 onClick={() => (window.location.href = "/")}>.logo</h2>
+        <img src={sign}/>
         </div>
 
         {/* Hamburger (always visible) */}
-        <div className="hamburger" onClick={toggleSidebar}>
+        {/* <div className="hamburger" onClick={toggleSidebar}>
           <div></div>
           <div></div>
           <div></div>
-        </div>
+        </div> */}
       </nav>
 
       {/* Sidebar */}
-      <div className={`mobile-sidebar ${sidebarOpen ? 'open' : ''}`}>
+      {/* <div className={`mobile-sidebar ${sidebarOpen ? 'open' : ''}`}>
         <ul>
           <li><a href="/" onClick={toggleSidebar}>Works</a></li>
           <li><a href="/" onClick={toggleSidebar}>Skills</a></li>
           <li><a href="/" onClick={toggleSidebar}>Contact</a></li>
           <li><a href="/" onClick={toggleSidebar}>Services</a></li>
         </ul>
-      </div>
+      </div> */}
 
       {/* Overlay */}
-      {sidebarOpen && <div className="overlay" onClick={toggleSidebar}></div>}
+      {/* {sidebarOpen && <div className="overlay" onClick={toggleSidebar}></div>} */}
     </>
   );
 }
