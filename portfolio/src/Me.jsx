3bcb4a1e029970c7e9afc0ba291222ecb02ff8d3
pranush-reddy <pranush.reddy0102@gmail.com>
2025-10-29 10:05:35 +0530

    About section add

---
+++ portfolio/src/Me.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Photocard from "./assets/Me.jpg";
+import "./Me.css";
+
+function Me() {
+  return (
+    <div className="About">
+      <div className="image">
+        <img src={Photocard} alt="Me" />
+      </div>
+      <div className="descrip">
+        <h3>About Me</h3>
+        <p>
+         I’m Yashwant — an editor, photographer, and cinematographer driven by a passion for visual storytelling.
+I love turning moments into emotions and ideas into cinematic experiences. Whether behind the lens or in the edit suite, I aim to create visuals that inspire, move, and stay with people long after they’re seen.
+          </p>
+      </div>
+    </div>
+  );
+}
+
+export default Me;
