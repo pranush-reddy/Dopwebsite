1ac58abb0460e408f11fab45f5f3c2071ba4ebf3
pranush-reddy <pranush.reddy0102@gmail.com>
2025-10-29 11:23:48 +0530

    content upload

--- portfolio/src/Landing.jsx
+++ portfolio/src/Landing.jsx
@@ -5,20 +5,35 @@ import LightRays from './LightRays';
 import Contact from './Contact.jsx';
 import Me from './Me.jsx';
 import Skills from './Skills.jsx';
+import Vido from './assets/drop.mp4';
 import Footer from './Footer.jsx';
 function Landing() {
+  console.log(Vido)
   return (<>
   
-<div style={{ width: '100vw', height: '100vh', position: 'absolute', backgroundColor:'#060010' }}>
+{/* <div style={{ width: '100vw', height: '100vh', position: 'absolute', backgroundColor:'#060010' }}>
   <LightRays
     raysOrigin="top-center" raysColor="#ffffffff" raysSpeed={1.5} lightSpread={0.8} rayLength={1.2} followMouse={true} mouseInfluence={0.1} noiseAmount={0.1} distortion={0.05} className="custom-rays"/>
-</div>
+</div> */}
+
+   <div className="video-background">
   <App/>
+      <video
+        className="video-bg"
+        autoPlay
+        muted
+        loop
+        preload='auto'
+        playsInline
+      >
+        <source src={Vido} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     <div className='main'>
         <h3>B YASHWANT KUMAR</h3>
         <h5 id='qual'>Video Editor - Cinematography - Designs</h5>
         <button>connect <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"></path> <path opacity="0.5" d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"></path> </g></svg></button>
-    </div>
+    </div></div>
     <Me/>
     <Skills/>
     <Contact/>
--- portfolio/src/Me.jsx
+++ portfolio/src/Me.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
+import React from "react";
 import Photocard from "./assets/Me.jpg";
 import "./Me.css";
 
 function Me() {
   return (
-    <div className="About">
-      <div className="image">
-        <img src={Photocard} alt="Me" />
-      </div>
+    <div className="About" style={{ backgroundImage: `url(${Photocard})` }}>
+      <div className="overlay"></div>
       <div className="descrip">
         <h3>About Me</h3>
         <p>
-         I’m Yashwant — an editor, photographer, and cinematographer driven by a passion for visual storytelling.
-I love turning moments into emotions and ideas into cinematic experiences. Whether behind the lens or in the edit suite, I aim to create visuals that inspire, move, and stay with people long after they’re seen.
-          </p>
+          I’m Yashwant — an editor, photographer, and cinematographer driven by a passion for visual storytelling.
+          I love turning moments into emotions and ideas into cinematic experiences. Whether behind the lens or in the
+          edit suite, I aim to create visuals that inspire, move, and stay with people long after they’re seen.
+        </p>
       </div>
     </div>
   );
